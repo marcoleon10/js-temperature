b51eda9261747633c6425caf8559a5ef190bbc49
Marco Antonio Leon Rodriguez <marco.leon11@uabc.edu.mx>
2023-12-13 15:57:57 -0800

    Used the web application Youtube and created 20 variables

---
+++ scripts/practice.js
@@ -0,0 +1,98 @@
+//Class 1 - Assignment 1
+//WEB APPLICATION: YOUTUBE
+//Variables
+let videoTitle = "The importance of Documenting my (boring) life";
+
+let videoCreator = "See Min Ng";
+let videoCaption = true;
+let videoVolume = 50;
+let videoPause = false;
+let videoPlay = true;
+let videoLanguage = "English";
+let videoQuality = "1080p";
+let videoShowCompleteScreen = false;
+let videoLength = "5:33"; //because it's using an specific format, it should be a string
+let videoViews = 1500;
+let videoQuantityLikes = 162;
+let videoQuantityDislikes = 0;
+let videoQuantityComments = 40;
+let videoShares = 0;
+let videoSubscribers = 6003;
+let videoButtonSubscribe = false;
+let videoDescription = "hi my friends~* this lil video is not only a reminder to slow down and appreciate your present moments more, it is also a mini tribute to New York City... the city that changed everything for me. It taught me to believe in myself and never give up on my dreams, because everything is possible. one day I'll make a dedicated video for that while being in NYC itself, heh. but I hope I could manage to pass off the transformative energies the city has shown me. ";
+let videoComment1 = "this is such an amazing film🤯the compositions, the colours, the script, the visuals, everything ties in so neatly and aptly. the themes explored are so relevant to creatives too! extremely inspiring ❤️‍🔥 i’m going to binge the rest of your videos now HAHA";
+let videoCommentsEnabled = true;
+
+//Display values using document.write
+document.write(`<h1> ${videoTitle}</h1>`);
+document.write(`<h2> ${videoCreator}</h2>`);
+document.write(`<p> ${videoDescription}</p>`);
+document.write(`<p> ${videoLength} minutes</p>`);
+document.write(`<p> ${videoViews} views</p>`);
+document.write(`<p> ${videoQuantityLikes} likes</p>`);
+document.write(`<p> ${videoQuantityDislikes} dislikes</p>`);
+document.write(`<p> ${videoQuantityComments} comments</p>`);
+document.write(`<p> ${videoShares} share</p>`);
+document.write(`<p> ${videoSubscribers} subscribers</p>`);
+document.write(`<p> Comment: ${videoComment1}</p>`);
+document.write(`<p> Video Comments Enable: ${videoCommentsEnabled}</p>`);
+document.write(`<p> Video Language: ${videoLanguage}</p>`);
+document.write(`<p> Video Quality: ${videoQuality}</p>`);
+document.write(`<p> Video Show Complete Screen: ${videoShowCompleteScreen}</p>`);
+document.write(`<p> Video Button Subscribe: ${videoButtonSubscribe}</p>`);
+document.write(`<p> Video Pause: ${videoPause}</p>`);
+document.write(`<p> Video Play: ${videoPlay}</p>`);
+document.write(`<p> Video Caption: ${videoCaption}</p>`);
+document.write(`<p> Video Volume: ${videoVolume}%</p>`);
+
+
+
+
+
+
+
+
+
+
+
+
+//                  PRACTICE
+//here we will use JS code
+//alert("Hello World cohort 44")
+//console.log("Hello World")
+//console.warn("This is a warning")
+//console.error("This is an error")
+
+/*
+//professor's info
+let professorName = "Samantha";
+let myGrade = 4.0;
+let isProfessor = true;
+
+const URL = "https://sdgku.instructure.com/courses/974/assignments/11256?module_item_id=37358"
+
+//Displaying values
+console.log("My name is: " + professorName);
+console.log("FSDI 101 grade: " + myGrade);
+console.log("Active: " + isProfessor);
+
+//student's info
+let studentName = "Marco";
+let studentGrade = 3.0;
+let isStudent = true;
+
+//Displaying values
+console.log("My name is: " + studentName);
+console.log("FSDI 101 grade: " + studentGrade);
+console.log("Active: " + isStudent);
+
+let num1 = 10;
+let num2 = 5;
+console.log("Multiplication: ", num1 * num2);
+console.log("Division: ", num1 / num2);
+console.log("Addition: ", num1 + num2);
+console.log("Subtraction: ", num1 - num2);
+
+
+//Examples of using document.write
+document.write(`<h1> ${studentName}</h1>`);*/
